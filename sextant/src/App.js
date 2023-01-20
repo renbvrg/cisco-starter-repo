43c77a1ccb02f2d954e7656df1878a1fcb67904c
Rana Ibrahimova <ranaibrahimova@shhh.local>
2023-01-20 15:35:33 +0100

    added a new component to collect and display the user’s public IP addresses

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,14 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ChildComponent1 = < div > < p > Child Component 1 < /p></div > ;
 const ChildComponent2 = < div > < p > Child Component 2 < /p></div > ;
 
+const IpAddress = ({ ipType }) => {
+    const [ipAddress, setIpAddress] = useState('');
+
+    useEffect(() => {
+        let url = 'https://api.ipify.org?format=json';
+        if (ipType === 'ipv6') {
+            url = 'https://api6.ipify.org?format=json';
+        }
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setIpAddress(data.ip))
+            .catch(err => console.log(err));
+    }, []);
+
+    return ( <
+        div >
+        <
+        h2 > Your IP Address: < /h2> <
+        p > { ipAddress } < /p> < /
+        div >
+    );
+}
+
 const App = () => {
-    const exhibit = React.createElement(Exhibit, { heading: "My Exhibit" },
-        ChildComponent1,
-        ChildComponent2
+    const exhibit1 = React.createElement(Exhibit, { heading: "IPv4 Address" },
+        React.createElement(IpAddress, { ipType: "ipv4" })
+    );
+    const exhibit2 = React.createElement(Exhibit, { heading: "IPv6 Address" },
+        React.createElement(IpAddress, { ipType: "ipv6" })
     );
     return ( <
         div className = "App" >
@@ -21,21 +46,19 @@ const App = () => {
         <
         h1 > Welcome to Sextant < /h1> <
         p >
-        Edit < code > src / App.js < /code> and save to reload. <
-        /p> <
+        Edit < code > src / App.js < /code> and save to reload. < /
+        p > <
         a className = "App-link"
         href = "https://reactjs.org"
         target = "_blank"
         rel = "noopener noreferrer" >
         Learn React <
-        /a>
-
-        <
-        /header> { exhibit } <
+        /a> < /
+        header > { exhibit1 } { exhibit2 } <
         /div>
-
     );
-}
+};
+
 
 const Exhibit = ({ heading, children }) => {
     const exhibit = React.createElement('div', { className: 'exhibit' },
